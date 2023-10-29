27dd95e0582acfcc72ffac9d5c2e702bbb174fa5
Christian Roberto <christian.roberto.g@gmail.com>
2023-10-28 19:01:41 -0600

    Implementar todos los Lifecycle Hooks

--- src/app/products/pages/product/product.component.ts
+++ src/app/products/pages/product/product.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit, AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges, OnDestroy,
+  OnInit, SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'products-product-page',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+// 1. Se inicia implementando los ciclos de vida: en este caso son todos los de la linea siguiente.
+export class ProductComponent implements OnInit, OnChanges, DoCheck, AfterContentInit,AfterContentChecked,AfterViewInit, AfterViewChecked, OnDestroy {
+
+  // 3. Se crea una propiedad para hacer un cambio en el template
+  public isProductVisible:boolean = false;
+
+
+  // 2. Despues implementar todas las interfaces creadas. Tambien un constructor este tambien es un ciclo de vida
+  constructor() {
+    console.log('constructor')
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked')
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit')
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked')
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit')
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck')
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log({changes})
+    console.log('ngOnChanges')
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy')
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit')
+  }
 
 }
