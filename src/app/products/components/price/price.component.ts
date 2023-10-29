7472ee2a33674cabc69897345bcf0adc3aa7518c
Christian Roberto <christian.roberto.g@gmail.com>
2023-10-28 21:09:11 -0600

    ngOnDestroy

--- src/app/products/components/price/price.component.ts
+++ src/app/products/components/price/price.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
+import {interval, Subscription} from "rxjs";
 
 @Component({
   selector: 'products-price',
@@ -9,10 +10,16 @@ export class PriceComponent implements  OnInit, OnChanges, OnDestroy{
   // 4. Implementar un input para que se active el onchanges
   @Input()
   public price:number = 0;
+  // 10 Crear una propiedad que almacenará la suscripción
+  public interval$?: Subscription;
 
   // 5. Hacer el resto de implementaciones
   ngOnInit(): void {
-    console.log('Componente Hijo: ngOnInit')
+    console.log('Componente Hijo: ngOnInit');
+
+    // 9. Crear una suscripción que emita valores infinitos
+    // 11. Iniciar la suscripcion
+    this.interval$ = interval(1000).subscribe(value => console.log(`Tiks: ${value}`))
   }
   ngOnChanges(changes: SimpleChanges): void {
     console.log('Componente Hijo: ngOnChanges');
@@ -21,6 +28,8 @@ export class PriceComponent implements  OnInit, OnChanges, OnDestroy{
 
   ngOnDestroy(): void {
     console.log('Componente Hijo: ngOnDestroy')
+    // 12. Destruir la suscripción
+    this.interval$?.unsubscribe();
   }
 
 }
